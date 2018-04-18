87c2c93765426bc6513d02de94e822b046d15f00
Thortilla <33997787+ThorTila@users.noreply.github.com>
2018-04-18 15:53:49 +0200

    add some styles and simple routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import registerServiceWorker from './registerServiceWorker';
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
+import routes from './routes';
+import registerServiceWorker from './registerServiceWorker';
+import './country.css';
 
 render(
     <Provider store={store}>
-        <div>
-            <DevTools/>
-            <h1>Inicjalizacja projektu</h1>
-        </div>
+        <Router history={hashHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root')
 );
+
 registerServiceWorker();
