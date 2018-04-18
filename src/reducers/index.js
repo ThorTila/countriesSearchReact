01f83960e519a5d341295b8f7d80b7e417573f8a
Thortilla <33997787+ThorTila@users.noreply.github.com>
2018-04-19 00:15:37 +0200

    publish

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import countriesReducer  from './countries-reducer';
-
-const reducers = combineReducers({
-    countriesReducer
-});
-
-export default reducers;
