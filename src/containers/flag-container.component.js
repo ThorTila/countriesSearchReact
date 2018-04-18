3d7535ab187d9f7e3ba3540766eb81354c9b18f9
Thortilla <33997787+ThorTila@users.noreply.github.com>
2018-04-18 17:20:38 +0200

    add option to delete country

--- src/containers/flag-container.component.js
+++ src/containers/flag-container.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CountryFlagList from '../presentational/flag-list.component';
-import { getCountries, searchCountries } from '../actions/actions-countries';
+import { getCountries, searchCountries, deleteCountry } from '../actions/actions-countries';
 
 class CountryFlagContainer extends Component {
     constructor(props) {
@@ -16,7 +16,11 @@ class CountryFlagContainer extends Component {
 
     search(event) {
         this.props.dispatch(searchCountries(event.target.value));
-}
+    }
+
+    deleteCountry(id) {
+        this.props.dispatch(deleteCountry(id));
+    }
 
     render() {
         return (
@@ -24,7 +28,7 @@ class CountryFlagContainer extends Component {
             <div className="search text-center">
                 <input type="text" onChange={this.search.bind(this)}/>
             </div>
-            <CountryFlagList countries={this.props.visibleCountries} />
+            <CountryFlagList deleteCountry={this.deleteCountry.bind(this)} countries={this.props.visibleCountries} />
         </div>
         )
     }
